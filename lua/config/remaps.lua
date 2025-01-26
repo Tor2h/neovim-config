2edc25324735d2826a452bc369df4cd65c6617a9
Tor2H <tor.holm@live.dk>
2025-01-26 21:31:30 +0100

    correction

--- lua/config/remaps.lua
+++ lua/config/remaps.lua
@@ -11,3 +11,5 @@ vim.keymap.set("v", "<leader>y", "\"+y")
 
 vim.keymap.set("n", "<leader>d", "\"_d")
 vim.keymap.set("v", "<leader>d", "\"_d")
+
+vim.keymap.set("n", "<leader>i", "<C-]>", { noremap = true, silent = true })
