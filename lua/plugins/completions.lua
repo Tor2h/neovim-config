c855a07adfc24992842bd469807ae1bd125c5be1
Tor Holm <Tor.Holm@logimatic.dk>
2025-03-04 15:56:08 +0100

    completion snippets first. missing search completion.

--- lua/plugins/completions.lua
+++ lua/plugins/completions.lua
@@ -9,10 +9,48 @@ return {
 			"rafamadriz/friendly-snippets",
 		},
 	},
+
+	{
+		"hrsh7th/cmp-cmdline",
+		lazy = false,
+		priority = 100,
+		config = function()
+			local cmp = require("cmp")
+			require("luasnip.loaders.from_vscode").lazy_load()
+
+			cmp.setup.cmdline(":", {
+				mapping = cmp.mapping.preset.cmdline(),
+				sources = cmp.config.sources({
+					{ name = "path" },
+				}, {
+					{ name = "cmdline" },
+				}),
+				matching = { disallow_symbol_nonprefix_matching = false },
+			})
+		end,
+	},
+
+	-- {
+	-- 	"hrsh7th/cmp-buffer",
+	-- 	lazy = false,
+	-- 	priority = 100,
+	-- 	config = function()
+	-- 		local cmp = require("cmp")
+	-- 		require("luasnip.loaders.from_vscode").lazy_load()
+	--
+	-- 		cmp.setup.cmdline({ "/", "?" }, {
+	-- 			mapping = cmp.mapping.preset.cmdline(),
+	-- 			sources = {
+	-- 				{ name = "buffer" },
+	-- 			},
+	-- 		})
+	-- 	end,
+	-- },
+
 	{
 		"hrsh7th/nvim-cmp",
 		lazy = false,
-		priority = 100,
+		priority = 200,
 		config = function()
 			local cmp = require("cmp")
 			require("luasnip.loaders.from_vscode").lazy_load()
@@ -35,48 +73,12 @@ return {
 					["<CR>"] = cmp.mapping.confirm({ select = true }), -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
 				}),
 				sources = cmp.config.sources({
-					{ name = "nvim_lsp" },
 					{ name = "luasnip" }, -- For luasnip users.
-				}, {
-					{ name = "buffer" },
-				}),
-			})
-		end,
-	},
-
-	{
-		"hrsh7th/cmp-cmdline",
-		lazy = false,
-		priority = 100,
-		config = function()
-			local cmp = require("cmp")
-			require("luasnip.loaders.from_vscode").lazy_load()
-
-			cmp.setup.cmdline(":", {
-				mapping = cmp.mapping.preset.cmdline(),
-				sources = cmp.config.sources({
+					{ name = "nvim_lsp" },
 					{ name = "path" },
 				}, {
-					{ name = "cmdline" },
-				}),
-				matching = { disallow_symbol_nonprefix_matching = false },
-			})
-		end,
-	},
-
-	{
-		"hrsh7th/cmp-buffer",
-		lazy = false,
-		priority = 100,
-		config = function()
-			local cmp = require("cmp")
-			require("luasnip.loaders.from_vscode").lazy_load()
-
-			cmp.setup.cmdline({ "/", "?" }, {
-				mapping = cmp.mapping.preset.cmdline(),
-				sources = {
 					{ name = "buffer" },
-				},
+				}),
 			})
 		end,
 	},
