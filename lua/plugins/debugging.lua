e325cb208d56cc615afa717a4f684c9e4ac2abf4
Tor Holm <tor.holm@live.dk>
2025-04-30 21:35:01 +0200

    debug c# almost

--- lua/plugins/debugging.lua
+++ lua/plugins/debugging.lua
@@ -3,7 +3,6 @@ return {
 		"mfussenegger/nvim-dap",
 		dependencies = {
 			"rcarriga/nvim-dap-ui",
-			"theHamsta/nvim-dap-virtual-text",
 			"nvim-neotest/nvim-nio",
 			"williamboman/mason.nvim",
 		},
@@ -46,22 +45,16 @@ return {
 
 			require("dapui").setup()
 
-			require("nvim-dap-virtual-text").setup({
-				-- This just tries to mitigate the chance that I leak tokens here. Probably won't stop it from happening...
-				display_callback = function(variable)
-					local name = string.lower(variable.name)
-					local value = string.lower(variable.value)
-					if name:match("secret") or name:match("api") or value:match("secret") or value:match("api") then
-						return "*****"
-					end
-
-					if #variable.value > 15 then
-						return " " .. string.sub(variable.value, 1, 15) .. "... "
-					end
-
-					return " " .. variable.value
-				end,
-			})
+			dap.configurations.cs = {
+				{
+					type = "coreclr",
+					name = "launch - netcoredbg",
+					request = "launch",
+					program = function()
+						return vim.fn.input(vim.fn.getcwd():gsub("\\", "/"), vim.fn.getcwd() .. "/bin/Debug/", "file")
+					end,
+				},
+			}
 
 			vim.keymap.set("n", "<space>?", function()
 				require("dapui").eval(nil, { enter = true })
