77816f975a28ef0bcb74cfee8c1aa5829cf75982
Tor Holm <tor.holm@live.dk>
2026-03-31 17:09:35 +0200

    fix merge

--- lua/plugins/db.lua
+++ lua/plugins/db.lua
@@ -2,4 +2,12 @@ return {
 	"tpope/vim-dadbod",
 	"kristijanhusak/vim-dadbod-completion",
 	"kristijanhusak/vim-dadbod-ui",
+	"Kurren123/mssql.nvim",
+	opts = {
+		-- optional
+		keymap_prefix = "<leader>æ",
+		max_rows = 1000,
+	},
+	-- optional
+	dependencies = { "folke/which-key.nvim" },
 }
